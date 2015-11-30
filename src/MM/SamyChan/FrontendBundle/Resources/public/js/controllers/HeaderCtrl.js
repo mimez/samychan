7116034f3cb4d58958383af2393b4a01ca2d3253
Michael Mezger <michael@michaelmezger.de>
2015-11-30 19:32:53 +0000

    HeaderCtrl for download modal

--- src/MM/SamyChan/FrontendBundle/Resources/public/js/controllers/HeaderCtrl.js
+++ src/MM/SamyChan/FrontendBundle/Resources/public/js/controllers/HeaderCtrl.js
@@ -2,9 +2,11 @@ samyChanApp.controller('HeaderCtrl', function($scope, backendUrlGenerator, nav) 
     $scope.uploadUrl = backendUrlGenerator.buildUploadUrl();
     $scope.downloadUrl = backendUrlGenerator.buildDownloadUrl();
 
+    $scope.download = function() {
+        $('#modal-download').foundation('reveal', 'open');
+    }
+
     nav.loadItems(function(items) {
         $scope.navItems = items;
     });
-
-
 });
