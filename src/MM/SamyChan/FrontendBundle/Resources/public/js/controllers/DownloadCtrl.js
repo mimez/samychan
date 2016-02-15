8a48e2040bf1077fa82a040c8ed173079a05566a
Michael Mezger <michael@michaelmezger.de>
2016-02-15 20:11:53 +0000

    donate-button integrated

--- src/MM/SamyChan/FrontendBundle/Resources/public/js/controllers/DownloadCtrl.js
+++ src/MM/SamyChan/FrontendBundle/Resources/public/js/controllers/DownloadCtrl.js
@@ -1,5 +1,7 @@
 samyChanApp.controller('DownloadCtrl', function($scope, eventTracker, $timeout) {
 
+    $scope.currentStep = 1;
+
     $scope.startDownloader = function() {
         $('#modal-download').foundation('reveal', 'open');
         eventTracker.track('Downloader', 'open');
@@ -7,6 +9,11 @@ samyChanApp.controller('DownloadCtrl', function($scope, eventTracker, $timeout) 
 
     $scope.download = function() {
         eventTracker.track('Downloader', 'download');
+        $scope.currentStep = 2;
+    }
+
+    $scope.donate = function() {
+        eventTracker.track('Downloader', 'donate');
     }
 
     $("#download-button").click($scope.startDownloader);
